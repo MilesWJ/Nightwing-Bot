2c25bb083b6afbe1417f99bd2611b25fbabd2142
Miles James <milesw.james@gmail.com>
2021-03-21 19:34:03 -0700

    Add files via upload

---
+++ Nightwing-Bot/main.py
@@ -0,0 +1,60 @@
+import praw
+import random
+import time
+
+reddit = praw.Reddit(
+    client_id="3D8wQy3XcNc0SQ",
+    client_secret="KGA6GZyJrmfsXcqtAq8GJzVPlkVxfQ",
+    user_agent="<<console:Nightwing-Bot>>",
+    username="Nightwing-Bot",
+    password="MilesWJames062704"
+)
+
+
+def get_quote():
+    with open("quotes.txt", "r") as file:
+        quotes = file.read().splitlines()
+        return random.choice(quotes)
+
+
+def post():
+    while True:
+        ask = str(input("\nEnter the subreddit name: "))
+        subreddit = reddit.subreddit(ask)
+        print(f"Searching for {ask} subreddit...")
+
+        try:
+            print(f"Subreddit r/{ask} found.")
+            for post in subreddit.hot(limit=10):
+                for comment in post.comments:
+                    if hasattr(comment, "body"):
+                        comment_lower = comment.body.lower()
+                        if "nightwing" in comment_lower:
+                            print("Replying to a post...")
+                            quote = get_quote()
+                            comment.reply(quote)
+                            print("Reply sent.")
+                            time.sleep(900)
+            break
+
+        except:
+            print(f"\nNo subreddit found for {ask}.")
+            continue
+
+
+def main():
+    while True:
+        ask = str(input(
+            "\nWould you like to run the Nightwing Bot? [Yes or No]: ")).lower()
+        if "yes" in ask:
+            post()
+            break
+        elif "no" in ask:
+            print("\nMayer another time...")
+            break
+        else:
+            print(f"Invalid response; {ask}. [Yes or No]")
+            continue
+
+
+main()
